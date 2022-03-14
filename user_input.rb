8b970b49eed2c7844e29ce4bc83c6d3873aa2806
Mitch Mills <mitchmillsgcp@gmail.com>
2022-03-14 14:49:47 -0500

    Redid exercises

--- user_input.rb
+++ user_input.rb
@@ -2,25 +2,29 @@ def valid_number?(number_string)
   number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
 
-def read_number
-  loop do
-    puts '>> Please enter a positive or negative integer:'
-    number = gets.chomp
-    return number.to_i if valid_number?(number)
-    puts '>> Invalid input. Only non-zero integers are allowed.'
-  end
-end
-
 first_number = nil
 second_number = nil
 
+puts ">> You will enter two integers, one positive and one negative."
 loop do
-  first_number = read_number
-  second_number = read_number
-  break if first_number * second_number < 0
-  puts '>> Sorry. One integer must be positive, one must be negative.'
-  puts '>> Please start over.'
+  loop do
+    print ">> Please enter your first integer: "
+    first_number = gets.chomp
+    break if valid_number?(first_number)
+    puts ">> Invalid input. Only non-zero integers are allowed."
+  end
+
+  loop do
+    print ">> Please enter your second integer: "
+    second_number = gets.chomp
+    break if valid_number?(second_number)
+    puts ">> Invalid input. Only non-zero integers are allowed."
+  end
+
+  break if first_number.to_i * second_number.to_i < 0
+  puts ">> One integer must be positive and one must be negative."
+  puts ">> Please start over."
 end
 
-sum = first_number + second_number
-puts "#{first_number} + #{second_number} = #{sum}"
+result = first_number.to_i + second_number.to_i
+puts "#{first_number} + #{second_number} = #{result}."
