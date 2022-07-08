6b6b43fe9c6000e9561f80bd6e80bd7f85b2aa2a
Mitch Mills <mitchmillsgcp@gmail.com>
2022-07-08 10:51:37 -0500

    Redid exercises

--- loops.rb
+++ loops.rb
@@ -1,5 +1,10 @@
-friends = ['Sarah', 'John', 'Hannah', 'Dave']
+friends = ["Sarah", "John", "Hannah", "Dave"]
 
-for friend in friends
-  puts "Hello,#{friend}!"
+# for friend in friends
+#   puts "Hello, #{friend}!"
+# end
+
+
+friends.each do |friend|
+  puts "Hello, #{friend}!"
 end
