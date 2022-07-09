20f11049581a358fa0f38894cad4f8e20fef5419
Mitch Mills <mitchmillsgcp@gmail.com>
2022-07-09 16:30:43 -0500

    Redid exercises

--- user_input.rb
+++ user_input.rb
@@ -2,29 +2,34 @@ def valid_number?(number_string)
   number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
 
+def get_number(order)
+  loop do
+    print ">> Please enter your #{order} integer: "
+    number = gets.chomp
+    return number.to_i if valid_number?(number)
+    puts ">> Invalid input. Only non-zero integers are allowed."
+    puts
+  end
+end
+
+
+puts ">> This program will add two integers that you provide."
+puts ">> One integer must be positive, and the other negative."
+puts
+
 first_number = nil
 second_number = nil
 
-puts ">> You will enter two integers, one positive and one negative."
 loop do
-  loop do
-    print ">> Please enter your first integer: "
-    first_number = gets.chomp
-    break if valid_number?(first_number)
-    puts ">> Invalid input. Only non-zero integers are allowed."
-  end
+  first_number = get_number("first")
+  second_number = get_number("second")
+  puts
 
-  loop do
-    print ">> Please enter your second integer: "
-    second_number = gets.chomp
-    break if valid_number?(second_number)
-    puts ">> Invalid input. Only non-zero integers are allowed."
-  end
-
-  break if first_number.to_i * second_number.to_i < 0
-  puts ">> One integer must be positive and one must be negative."
+  break if first_number * second_number < 0
+  puts ">> Sorry, one integer must be positive, and the other negative."
   puts ">> Please start over."
+  puts
 end
 
-result = first_number.to_i + second_number.to_i
-puts "#{first_number} + #{second_number} = #{result}."
+result = first_number + second_number
+puts ">> #{first_number} + #{second_number} = #{result}."
