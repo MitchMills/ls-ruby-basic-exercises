fc7eadba2a56a75349b22a2873cc67a97d10ca43
Mitch Mills <mitchmillsgcp@gmail.com>
2023-03-23 09:52:15 -0500

    Reworked loops and user input exercises

--- loops2.rb
+++ loops2.rb
@@ -1,5 +1,6 @@
+### 10 GREETING
 def greeting
-  puts "Hello!"
+  puts 'Hello!'
 end
 
 number_of_greetings = 2
@@ -8,3 +9,104 @@ while number_of_greetings > 0
   greeting
   number_of_greetings -= 1
 end
+
+### 9 FIRST TO FIVE
+# number_a = 0
+# number_b = 0
+
+# loop do
+#   number_a += rand(2)
+#   number_b += rand(2)
+#   puts "a: #{number_a}"
+#   puts "b: #{number_b}"
+#   puts
+#   # next unless number_a == 5 || number_b == 5
+#   # puts "5 was reached!"
+#   # break
+#   if number_a == 5 || number_b == 5
+#     puts "5 was reached!"
+#     break
+#   end
+# end
+
+### 8 ONLY EVEN
+# number = 0
+
+# until number == 10
+#   number += 1
+#   next if number.odd?
+#   puts number
+# end
+
+### 7 STOP COUNTING
+# 5.times do |index|
+#   puts index
+#   break if index < 7
+# end
+
+### 6 EMPTY THE ARRAY
+# names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+# loop do
+#   # puts names.shift
+#   puts names.pop
+#   break if names.empty?
+# end
+
+### 5 INSERT NUMBERS
+# numbers = []
+
+# loop do
+#   puts "Enter any number:"
+#   input = gets.chomp.to_i
+#   numbers << input
+#   break if numbers.size == 5
+# end
+# p numbers
+
+### 4 GET THE SUM
+# loop do
+#   puts "What does 2 + 2 equal?"
+#   answer = gets.chomp.to_i
+
+#   if answer == 4
+#     puts "That's correct!"
+#     break
+#   end
+  
+#   puts "Wrong answer. Try again!"
+# end
+
+### 3 CONDITIONAL LOOP
+# p process_the_loop = [true, false].sample
+
+# if process_the_loop
+#   loop do
+#     puts "Processed."
+#     break
+#   end
+# else
+#   puts "Not processed."
+# end
+
+### 2 CATCH THE NUMBER
+# loop do
+#   number = rand(100)
+#   puts number
+#   # break if number <= 10
+#   # break if number.between(0, 10)
+#   break if (0..10).include?(number)
+# end
+
+### 1 EVEN OR ODD?
+# count = 1
+
+# loop do
+#   if count.odd?
+#     puts "#{count} is odd!"
+#   else
+#     puts "#{count} is even!"
+#   end
+#   count += 1
+#   break if count > 5
+# end
--- user_input.rb
+++ user_input.rb
@@ -1,35 +1,149 @@
+### 10 OPPOSITES ATTRACT
 def valid_number?(number_string)
-  number_string.to_i.to_s == number_string && number_string.to_i != 0
+  (number_string.to_i.to_s == number_string) && (number_string.to_i != 0)
 end
 
-def get_number(order)
+def get_integer(integer, ordinal)
   loop do
-    print ">> Please enter your #{order} integer: "
-    number = gets.chomp
-    return number.to_i if valid_number?(number)
-    puts ">> Invalid input. Only non-zero integers are allowed."
-    puts
+    print "Please enter the #{ordinal} integer: "
+    integer = gets.chomp
+    return integer.to_i if valid_number?(integer)
+    puts "Invalid input. Only non-zero integers are allowed."
   end
 end
 
-
-puts ">> This program will add two integers that you provide."
-puts ">> One integer must be positive, and the other negative."
-puts
-
-first_number = nil
-second_number = nil
+# main program loop
+puts "Please enter two integers."
+puts "One must be positive and the other negative."
+integer1 = nil
+integer2 = nil
 
 loop do
-  first_number = get_number("first")
-  second_number = get_number("second")
-  puts
+  integer1 = get_integer(integer1, "first")
+  integer2 = get_integer(integer2, "second")
 
-  break if first_number * second_number < 0
-  puts ">> Sorry, one integer must be positive, and the other negative."
-  puts ">> Please start over."
-  puts
+  break if (integer1.to_i * integer2.to_i) < 0
+  puts "Sorry. One integer must be positive and the other negative."
+  puts "Please start over."
 end
 
-result = first_number + second_number
-puts ">> #{first_number} + #{second_number} = #{result}."
+sum = integer1 + integer2
+puts "#{integer1} + #{integer2} = #{sum}"
+
+### 9 LAUNCH SCHOOL PRINTER II
+# loop do
+#   puts ">> How many output lines do you want?"
+#   print "Enter a number >= 3 (Q to quit): "
+#   answer = gets.chomp
+
+#   break if answer.downcase == 'q'
+
+#   if answer.to_i >= 3
+#     answer.to_i.times do
+#       puts "Launch School!"
+#     end
+#     next
+#   end
+
+#   puts "That's not enough lines."
+# end
+
+### 8 DIVIDING NUMBERS
+# def valid_number?(number_string)
+#   number_string.to_i.to_s == number_string
+# end
+
+# numerator = nil
+# denominator = nil
+
+# loop do
+#   print "Please enter the numerator: "
+#   numerator = gets.chomp
+#   break if valid_number?(numerator)
+#   puts "Invalid input. Only integers are allowed."
+# end
+
+# loop do
+#   print "Please enter the denominator: "
+#   denominator = gets.chomp
+
+#   if valid_number?(denominator)
+#     if denominator.to_i == 0
+#       puts "Invalid input. A denominator of 0 is not allowed."
+#       next
+#     end
+#     break
+#   else
+#     puts "Invalid input. Only integers are allowed."
+#   end
+# end
+
+# puts "#{numerator} divided by #{denominator} equals #{numerator.to_i / denominator.to_i}"
+
+### 7 USER NAME AND PASSWORD
+# USER_NAME = "admin"
+# PASSWORD = "SecreT"
+
+# loop do
+#   print ">> Please enter user name: "
+#   name_attempt = gets.chomp
+#   print ">> Please enter your password: "
+#   password_attempt = gets.chomp
+
+#   break if name_attempt == USER_NAME && password_attempt == PASSWORD
+#   puts ">> Authorization failed!"
+# end
+# puts ">> Welcome!"
+
+### 6 PASSWORDS
+# PASSWORD = "SecreT"
+
+# loop do
+#   puts ">> Please enter your password:"
+#   attempt = gets.chomp
+#   break if attempt == PASSWORD
+#   puts ">> Invalid password!"
+# end
+# puts ">> Welcome!"
+
+### 5 LAUNCH SCHOOL PRINTER I
+# number = 0
+# loop do
+#   puts "How many output lines do you want?"
+#   puts "Enter a number >= 3"
+#   number = gets.chomp.to_i
+#   break if number >= 3
+#   puts "That's not enough lines."
+# end
+
+# number.times do
+#   puts "Launch School!"
+# end
+
+
+### 4 PRINT SOMETHING II
+# answer = nil
+# loop do
+#   puts "Do you want me to print something? (y/n)"
+#   answer = gets.chomp.downcase
+#   break if %w(y n).include?(answer)
+#   puts "Invalid input! Please enter y or n"
+# end
+# puts 'something' if answer == 'y'
+
+### 3 PRINT SOMETHING I
+# puts "Do you want me to print something? (y/n)"
+# answer = gets.chomp.downcase
+# puts "something" if answer == 'y'
+
+### 2 YOUR AGE IN MONTHS
+# puts "What is your age in years?"
+# age_in_years = gets.chomp.to_i
+# age_in_months = age_in_years * 12
+# puts "You are at least #{age_in_months} months old."
+
+
+### 1 REPEAT AFTER ME
+# puts "Type anything you want:"
+# anything = gets.chomp
+# puts anything
