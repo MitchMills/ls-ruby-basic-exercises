ff784d9e87cfb0ccad99c46c228ec11ff288020e
Mitch Mills <mitchmillsgcp@gmail.com>
2023-03-24 09:40:09 -0500

    Reworked return exercises

--- return.rb
+++ return.rb
@@ -1,3 +1,4 @@
+### 10 TRICKY NUMBER
 def tricky_number
   if true
     number = 1
@@ -8,3 +9,80 @@ end
 
 puts tricky_number
 
+### 9 COUNTING SHEEP III
+# def count_sheep
+#   5.times do |sheep|
+#     puts sheep
+#     if sheep >= 2
+#       return
+#     end
+#   end
+# end
+
+# p count_sheep
+
+### 8 COUNTING SHEEP II
+# def count_sheep
+#   5.times do |sheep|
+#     puts sheep
+#   end
+#   10
+# end
+
+# puts count_sheep
+
+### 7 COUNTING SHEEP I
+# def count_sheep
+#   5.times do |sheep|
+#     puts sheep
+#   end
+# end
+
+# puts count_sheep
+
+### 6 BREAKFAST, LUNCH, OR DINNER? VI
+# def meal
+#   return 'Breakfast'
+#   'Dinner'
+#   puts 'Dinner'
+# end
+
+# puts meal
+
+### 5 BREAKFAST, LUNCH, OR DINNER? V
+# def meal
+#   'Dinner'
+#   puts 'Dinner'
+# end
+
+# p meal
+
+### 4 BREAKFAST, LUNCH, OR DINNER? IV
+# def meal
+#   puts 'Dinner'
+#   return 'Breakfast'
+# end
+
+# puts meal
+
+### 3 BREAKFAST, LUNCH, OR DINNER? III
+# def meal
+#   return 'Breakfast'
+#   'Dinner'
+# end
+
+# puts meal
+
+### 2 BREAKFAST, LUNCH, OR DINNER? II
+# def meal
+#   'Evening'
+# end
+
+# puts meal
+
+### 1 BREAKFAST, LUNCH, OR DINNER? I
+# def meal
+#   return 'Breakfast'
+# end
+
+# puts meal
