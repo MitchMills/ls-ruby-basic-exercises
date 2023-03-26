5f86c745d4794f46506816c529a61aada3e2abbc
Mitch Mills <mitchmillsgcp@gmail.com>
2023-03-26 09:34:50 -0500

    Reworked debugging exercises

--- debugging.rb
+++ debugging.rb
@@ -1,32 +1,90 @@
+### 10 WARRIORS AND WIZARDS
+player = {
+  strength:   10,
+  dexterity:  10,
+  stamina:    10,
+  charisma:   10
+}
+
+character_classes = {
+  warrior:  { strength:   20 },
+  thief:    { dexterity:  20 },
+  scout:    { stamina:    20 },
+  mage:     { charisma:   20 }
+}
+
+print 'Please type your class (warrior, thief, scout, mage): '
+choice = gets.chomp.downcase
+
+player = player.merge(character_classes[choice.to_sym])
+
+puts 'Your character stats:'
+puts player
+
+
+### 9 DIGIT PRODUCT
+# def digit_product(str_num)
+#   digits = str_num.chars.map { |n| n.to_i }
+#   product = 1
+
+#   digits.each do |digit|
+#     product *= digit
+#   end
+
+#   product
+# end
+
+# p digit_product('12345')
+
+### 8 COLORFUL THINGS
+# colors = ["red", "yellow", "purple", "green", "dark blue", "turquoise", "silver", "black"]
+# things = ["pen", "mouse pad", "coffee mug", "sofa", "surf board", "training mat", "notebook"]
+ 
+# colors.shuffle!
+# things.shuffle!
+
+# i = 0
+# loop do
+#   break if i >= [colors.length, things.length].min
+
+#   if i == 0
+#     puts 'I have a ' + colors[i] + ' ' + things[i] + '.'
+#   else
+#     puts 'And a ' + colors[i] + ' ' + things[i] + '.'
+#   end
+
+#   i += 1
+# end
+
 ### 7 ACCOUNT BALANCE
-balance = 0
+# balance = 0
 
-january = {
-  income: [ 1200, 75 ],
-  expenses: [ 650, 140, 33.2, 100, 26.9, 78 ]
-}
+# january = {
+#   income: [ 1200, 75 ],
+#   expenses: [ 650, 140, 33.2, 100, 26.9, 78 ]
+# }
 
-february = {
-  income: [ 1200 ],
-  expenses: [ 650, 140, 320, 46.7, 122.5 ]
-}
+# february = {
+#   income: [ 1200 ],
+#   expenses: [ 650, 140, 320, 46.7, 122.5 ]
+# }
 
-march = {
-  income: [ 1200, 10, 75 ],
-  expenses: [ 650, 140, 350, 12, 59.9, 2.5 ]
-}
+# march = {
+#   income: [ 1200, 10, 75 ],
+#   expenses: [ 650, 140, 350, 12, 59.9, 2.5 ]
+# }
 
-def calculate_balance(month)
-  plus = month[:income].sum
-  minus = month[:expenses].sum
-  plus - minus
-end
+# def calculate_balance(month)
+#   plus = month[:income].sum
+#   minus = month[:expenses].sum
+#   plus - minus
+# end
 
-[january, february, march].each do |month|
-  balance += calculate_balance(month)
-end
+# [january, february, march].each do |month|
+#   balance += calculate_balance(month)
+# end
 
-puts balance
+# puts balance
 
 ### 6 CONFUCIUS SAYS
 # def get_quote(person)
